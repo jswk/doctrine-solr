98c8163e8c5f6a8ca4c4683a870a810e3be6f051
Jakub Sawicki <jurekss@gmail.com>
2012-10-05 14:50:59 +0200

    Moved phpunit.xml to root. Added MongoDB\ODM to required packages.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,18 @@
 
 $loader = require_once __DIR__.'/../vendor/autoload.php';
 
-$loader->add('Unit\\Doctrine\\Solr', __DIR__);
+$loader->add('Doctrine\\Tests\\Solr', __DIR__);
+//$loader->add('Doctrine\\ODM\\MongoDB\\Tests', __DIR__.'/../vendor/doctrine/mongodb-odm/tests');
 
 // use statements
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\EventManager;
+use Doctrine\Solr\Subscriber\MongoDBSubscriber;
 
-AnnotationRegistry::registerFile(__DIR__ . '/../lib/Doctrine/Search/Mapping/Annotations/DoctrineAnnotations.php');
-AnnotationRegistry::registerFile(__DIR__ . '/../vendor/doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php');
+AnnotationRegistry::registerAutoloadNamespace("Doctrine\\Solr\\Mapping\\Annotations");
+//AnnotationRegistry::registerFile(__DIR__ . '/../lib/Doctrine/Search/Mapping/Annotations/DoctrineAnnotations.php');
+//AnnotationRegistry::registerFile(__DIR__ . '/../vendor/doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php');
+
+$em = new EventManager();
+
+$em->addEventSubscriber(new MongoDBSubscriber);
